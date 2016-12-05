c6dfdedc515efe6b67fc6640e3fdf43bd5ae63af
Lorenzo <Lorenzo@192.168.1.74>
2016-12-05 21:49:22 +0100

    Seconda fase dell'implementazione del database Riviste parzialmente il lancio e la gestione delle eccezioni e il rispetto di diverse invarianti della classe DDI. Ora il login avviene effettivamente per mezzo dell'interazione DDI-ControlloAccesso. Va aggiustata la notifica della PAI sul DB ed aggiungere degli avvisi al login non riuscito.

--- DDI/DDI.java
+++ DDI/DDI.java
@@ -16,29 +16,41 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class DDI {
-    
-    
+
     private static Properties ProprietàDB;
     private static List<Connection> RilasciaConnessioni;
-    
+
+    private static DDI instance = null;
+
+    public static DDI getInstance() {
+        if (instance == null) {
+            try {
+                instance = new DDI();
+            } catch (SQLException ex) {
+                Logger.getLogger(DDI.class.getName()).log(Level.SEVERE, "Errore: Fallimento nell'istaurazione della connessione con il db", ex);
+                instance = null;
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(DDI.class.getName()).log(Level.SEVERE, "Errore: Driver per l'accesso al db non trovato", ex);
+                instance = null;
+            }
+        }
+        return instance;
+    }
+
     /**
      * Costruttore
      */
-    public DDI() {
+    private DDI() throws ClassNotFoundException, SQLException {
         RilasciaConnessioni = new ArrayList<Connection>();
         try {
             CaricaCredenziali();
-            CaricaDriver();
-        } catch (ClassNotFoundException e) {
-            System.out.println("Driver non trovato");
-            System.exit(1);
-        } catch (IOException e) {
-            System.out.println("Errore in GestioneConnessioni");
-            System.exit(2);
+        } catch (IOException ex) {
+            Logger.getLogger(DDI.class.getName()).log(Level.SEVERE, null, ex);
         }
+        CaricaDriver();
+        RilasciaConnessione(getConnection()); // test di connessione
     }
-    
-    
+
     private static void CaricaCredenzialiDefault() {
         ProprietàDB.setProperty("driver", "org.gjt.mm.mysql.Driver");
         ProprietàDB.setProperty("url", "jdbc:mysql://localhost/tunnel_db");
@@ -97,34 +109,50 @@ public class DDI {
             if (fileProperties != null) {
                 ProprietàDB.load(fileProperties);
             } else {
-                System.out.println("Loading default DB properties");
+                Logger.getLogger(DDI.class.getName()).log(Level.WARNING, "Attenzione: file delle credenziali non presente, verranno usate le credenziali di default");
                 CaricaCredenzialiDefault();
             }
         } catch (IOException e) {
-            System.out.println("Loading default DB properties");
+            Logger.getLogger(DDI.class.getName()).log(Level.WARNING, "Attenzione: Non è stato possibile aprire il file delle credenziali, verranno usate le credenziali di default", e);
             CaricaCredenzialiDefault();
         }
     }
-    
+
     /**
      * Effettua l'autenticazione dell'utente
+     *
      * @param username Username da verificare
      * @param password Password immessa
      * @return Livello autorizzazione oppure null
      */
     public Permesso readUtente(String username, String password) {
+        Permesso permesso = null;
+        if (password == null) {
+            password = "";
+        }
+        if (username == null) {
+            username = "";
+        }
         try {
             Connection con = getConnection();
             Statement stmt = con.createStatement();
             try {
                 /*QUERY FUNZIONANTE MANCA CONTROLLO CONTROLLO VERIDICITà CON I DATI INSERITI TEXTFIELD*/
-                ResultSet rs = stmt.executeQuery("select username,password from utente where Username = '"+username+"' and Password = "+password);
-                while (rs.next()) {
-                    System.out.println(rs.getString(1) + rs.getString(2));
+                ResultSet rs = stmt.executeQuery("select Livello_accesso from utente where Username = '" + username + "' and Password = '" + password + "'");
+                if (rs.next() == true) {
+                    switch (rs.getString(1)) {
+                        case "Operatore":
+                            permesso = Permesso.OPERATORE;
+                            break;
+                        case "Controllore":
+                            permesso = Permesso.CONTROLLORE;
+                            break;
+                    }
                     con.commit();
                 }
+                return permesso;
             } finally {
-                
+
                 if (stmt != null) {
                     stmt.close();
                 }
@@ -132,37 +160,39 @@ public class DDI {
                     RilasciaConnessione(con);
                 }
             }
-        }   catch (SQLException ex) {
+        } catch (SQLException ex) {
             Logger.getLogger(DDI.class.getName()).log(Level.SEVERE, "Si è verificato un problema con l'accesso al db", ex);
         }
-        
         return null;
     }
-    
+
     /**
      * Scrive nel database l'informazione dell'attivazione o disattivazione
      * della procedura antincendio
+     *
      * @param isAttiva <code>true</code> se la procedura viene attivata,
-     *                 <code>false</code> se la procedura viene disattivata
-     */    
+     * <code>false</code> se la procedura viene disattivata
+     */
     public void writePAI(boolean isAttiva) {
         try {
             /* Operazioni: scrive nel database lo stato di attivazione della procedura
             * antincendio e memorizza ora e data (con gli appositi operatori SQL)
             * ATTENZIONE: MANCA VERIFICA DI CONSISTENZA SULL'UTENTE CHE EFFETTUA TALE OPERAZIONE
-            */
-            String username=ControlloAccesso.getInstance().getUtenteLoggato();
-            if((username!=null && isAttiva==true) || (username==null && isAttiva==false))
+             */
+            String username = ControlloAccesso.getInstance().getUtenteLoggato();
+            if ((username != null && isAttiva == true) || (username == null && isAttiva == false)) {
                 return;
+            }
             Connection con = getConnection();
             Statement stmt = con.createStatement();
             con = getConnection();
             try {
                 /*QUERY FUNZIONANTE SU WORCKBENCH NON COMPLETATA VISTO CHE DEVE ESSERE CAMBIATA CON I TEXTFIELD DI JAVAFX */
-                if(username==null)
-                    username="";
-                stmt.executeUpdate("insert into attivazione_pai (username_responsabile) value ('"+username+"')");
-                
+                if (username == null) {
+                    username = "";
+                }
+                stmt.executeUpdate("insert into attivazione_pai (username_responsabile) value ('" + username + "')");
+
                 /*PreparedStatement stm = null;
                 stm = con.prepareStatement("insert into attivazione_pai (username_responsabile) value (\"?\")");
                 stm.setString(1, Username);
@@ -170,7 +200,7 @@ public class DDI {
                 }*/
                 con.commit();
             } finally {
-                
+
                 if (stmt != null) {
                     stmt.close();
                 }
@@ -178,7 +208,7 @@ public class DDI {
                     RilasciaConnessione(con);
                 }
             }
-        }   catch (SQLException ex) {
+        } catch (SQLException ex) {
             Logger.getLogger(DDI.class.getName()).log(Level.SEVERE, "Si è verificato un problema con l'accesso al db", ex);
         }
     }
